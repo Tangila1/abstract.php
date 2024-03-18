31da219ace5621b485b817ffb33aae58a9c27d1c
Tangila1 <tangila.akter.eden@gmail.com>
2024-03-18 16:09:37 +0600

    first commit

---
+++ abstract.php
@@ -0,0 +1,26 @@
+<?php
+abstract class User{ 
+    abstract public function getName();
+    abstract public function info();
+    public function desplay(){ 
+        echo "third";
+    }
+
+}
+
+class wdpf extends  User{ 
+    public function getName(){ 
+        echo "Done";
+    }
+    public function info(){ 
+        echo "second";
+    }
+    public function desplay(){ 
+        
+        echo "third";
+    }
+}
+
+$add = new wdpf;
+$add->getName();
+?>
